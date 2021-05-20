329d7603c6f58dbd149ba9470c30f834d88dde2e
ihongss <36065549+ihongss@users.noreply.github.com>
2021-05-20 11:47:12 +0900

    컨트롤러 기능 정의 완료

--- src/main/java/com/cos/blog/web/BoardController.java
+++ src/main/java/com/cos/blog/web/BoardController.java
@@ -13,14 +13,38 @@ public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		doProcess(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		doProcess(request, response);
 	}
 
 	public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		// 1. null과 공백 접근 금지 (유효성 검사)
+		if(request.getParameter("cmd") == null || request.getParameter("cmd").equals("")) {
+			return;
+		}
+		
+		String cmd = request.getParameter("cmd");
+		
+		// http://localhost:8080/blog/board?cmd=list
+		if(cmd.equals("list")) {
+			
+		}else if(cmd.equals("detail")) {
+			
+		}else if(cmd.equals("delete")) {
+			
+		}else if(cmd.equals("updateForm")) {
+			
+		}else if(cmd.equals("update")) {
+			
+		}else if(cmd.equals("saveForm")) {
+			
+		}else if(cmd.equals("save")) {
+			
+		}else if(cmd.equals("search")) {
+			
+		}
 	}
 }
--- src/main/java/com/cos/blog/web/UserController.java
+++ src/main/java/com/cos/blog/web/UserController.java
@@ -13,14 +13,37 @@ public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		doProcess(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		doProcess(request, response);
 	}
 
 	public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		// 1. null과 공백 접근 금지 (유효성 검사)
+		if(request.getParameter("cmd") == null || request.getParameter("cmd").equals("")) {
+			return;
+		}
+		
+		String cmd = request.getParameter("cmd");
+		
+		if(cmd.equals("joinForm")) {
+			
+		}else if(cmd.equals("join")) {
+			
+		}else if(cmd.equals("loginForm")) {
+			
+		}else if(cmd.equals("login")) {
+			
+		}else if(cmd.equals("updateForm")) {
+			
+		}else if(cmd.equals("update")) {
+			
+		}else if(cmd.equals("logout")) {
+			
+		}
 	}
 }
+
+
