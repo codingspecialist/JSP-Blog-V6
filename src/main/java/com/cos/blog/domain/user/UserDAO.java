dc90217377a59f2ea6edd5be563952da742ae4c8
ihongss <36065549+ihongss@users.noreply.github.com>
2021-05-27 09:11:10 +0900

    로그인 로직 수정

--- src/main/java/com/cos/blog/domain/user/UserDAO.java
+++ src/main/java/com/cos/blog/domain/user/UserDAO.java
@@ -36,8 +36,9 @@ public class UserDAO implements CrudDAO<User>{
 				user.setEmail(rs.getString("email"));
 				user.setAddress(rs.getString("address"));
 				user.setCreated(rs.getTimestamp("created"));
+				return user;
 			}
-			return user;
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
